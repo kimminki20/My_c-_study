8290cdaeff3745751bb5186b2228d8950943cb41
minki99 <mhn06159@naver.com>
2024-12-01 22:04:19 +0900

    12/1 lost_parentheses

---
+++ 11_25~12_1/24_12_01/Lost_parentheses.cpp
@@ -0,0 +1,35 @@
+//백준 1541 잃어버린 괄호
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string st; // 입력받을 식 
+    bool check = false; // - 판별
+    string num = ""; // 식에서 쓴 숫자
+    int min_aws = 0; // 식의 최소값
+    
+    cin >> st;
+    for(int i=0; i<= st.size(); i++){
+        //기호를 만나거나 식이 끝날 경우
+        if(st[i] == '-' || st[i] == '+' || i == st.size()){
+            if(check == true){ //앞서 '-'가 있다면 숫자 빼줌
+                min_aws -= stoi(num);
+                num = "";
+            } // '-'가 없으므로 숫자 더해줌
+        else if(check == false){
+            min_aws += stoi(num);
+            num = "";
+        }
+    } // 기호가 아니라 숫자인 경우
+    else{
+        num += st[i]; //문자열 형태로 숫사 지정
+    }
+    if(st[i] == '-'){
+        check = true;
+        }
+    }
+    cout << min_aws;
+    return 0;
+}
