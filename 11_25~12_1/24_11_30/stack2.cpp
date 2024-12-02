e4c3d03ed7ec155a61a77eb094edca87354b369d
minki99 <mhn06159@naver.com>
2024-12-02 13:49:53 +0900

    endl과 \n의 속도 \n이 더 빠름

--- 11_25~12_1/24_11_30/stack2.cpp
+++ 11_25~12_1/24_11_30/stack2.cpp
@@ -21,31 +21,31 @@ int main(){
         case 2: //맨 위의 정수 빼고 출력
             if(!st.empty()){ //비어있지않으면 출력
             int b = st.top();
-            cout << b << endl;
+            cout << b << '\n';
             st.pop();}
             else{ //값이 비어있으면 -1 출력
-                cout << "-1" << endl;
+                cout << "-1" << '\n';
             }
             break;
         case 3: //들어있는 정수의 개수 즉,사이즈
-            cout << st.size() << endl;
+            cout << st.size() << '\n';
             break;
         case 4: //st.empty() -> 비어있으면 1임을 생각 
             if(st.empty())
-                cout << "1" << endl;
-            else cout << "0" << endl;
+                cout << "1" << '\n';
+            else cout << "0" << '\n';
             break;
         case 5:  
         if(!st.empty()){ //비어있지않으면 출력
             int b = st.top();
-            cout << b << endl;
+            cout << b << '\n';
             }
         else{
-            cout << "-1" << endl;
+            cout << "-1" << '\n';
         }
             break;
         }
     }
     return 0;
 }
-//실행된 값은 맞게 나오나 시간이 오래걸리는 문제로 switch 보다는 while문으로 다시 풀음.
+// endl을 사용하는 것 보다 '\n'이 더 빠름
