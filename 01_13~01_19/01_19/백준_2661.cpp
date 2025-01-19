daaad91b976494b92c4405483cd97266eba5cbb6
minki99 <mhn06159@naver.com>
2025-01-19 15:02:38 +0900

    못푼 문제 따로 빼둠

---
+++ 01_13~01_19/01_19/백준_2661.cpp
@@ -0,0 +1,16 @@
+//백준 2661 좋은 수열
+//인접한 두 개의 부분 수열이 동일한 것을 나쁜 수열로 통칭
+//ex) 33,3(2121)323,(123123)213 은 나쁜 수열
+//숫자는 1,2,3으로만 이루어져있음
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> N; 
+    
+    return 0;
+}
